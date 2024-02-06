6000bb232ae039f1ceb43b7f16c310d4ded5d9ed
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2024-02-06 19:44:36 +0200

    s

--- myshop/settings.py
+++ myshop/settings.py
@@ -60,17 +60,22 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader'
+                
             ],
+            'loaders': [
+                
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+],
         },
+        
     },
 ]
 
